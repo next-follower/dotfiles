92c9f60c602796691fe7b8ec90f62e2986108d30
kkyx <feng3817x@gmail.com>
2026-06-18 22:28:51 +0800

    Sync config

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -25,6 +25,52 @@ return {
     },
   },
   {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      cmdline = {
+        enabled = true,
+        view = "cmdline_popup",
+        format = {
+          cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = "/", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = "?", lang = "regex" },
+          filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+          lua = { pattern = "^:%s*lua%s+", icon = "lua", lang = "lua" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = "?", lang = "vim" },
+        },
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "40%",
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+          },
+          border = {
+            style = "rounded",
+          },
+        },
+      },
+      messages = {
+        enabled = true,
+      },
+      popupmenu = {
+        enabled = true,
+      },
+      presets = {
+        command_palette = true,
+        long_message_to_split = true,
+      },
+    },
+  },
+  {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     event = { "BufReadPost", "BufNewFile" },
